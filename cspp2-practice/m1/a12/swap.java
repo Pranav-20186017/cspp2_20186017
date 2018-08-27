3d6e994624beaef26e5fee1e9a8720e01ed3e0c2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-27 21:47:43 +0530

    Module 1 Activity 12 Commit

---
+++ cspp2-practice/m1/a12/swap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public final class swap
+{
+	void swap_vals(int a,int b)
+	{
+		int temp=0;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("a = " +a+" "+"b = "+b);
+	}
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in); 
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		swap obj = new swap();
+		obj.swap_vals(a,b);
+	}
+}
