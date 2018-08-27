93a6e99dcb64d0b2a859e74863f6de2eb17b5f1f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-27 21:27:59 +0530

    Module 1 Assignment 3 Commit

--- cspp2-practice/m1/a3/Helloworld.java
+++ cspp2-practice/m1/a3/Helloworld.java
@@ -1,7 +1,17 @@
-class Helloworld
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello, World!");
-	}
+/**
+Hello World Class.
+*/
+final class Helloworld {
+    /**
+    Constructor for hc.
+    */
+    private Helloworld() {
+    }
+    /**
+        Main Method in java.
+        @param args of type string
+        */
+    public static void main(final String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
