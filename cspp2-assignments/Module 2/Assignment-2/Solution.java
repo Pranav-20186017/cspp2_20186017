8c7c1bc58006a5d9f9946e9dcf8ba51da11f5f6b
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-28 20:46:04 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module 2/Assignment-2/Solution.java
+++ cspp2-assignments/Module 2/Assignment-2/Solution.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
 import java.lang.Math;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	void rootsOfQuadraticEquation(int a, int b, int c)
-	{
-		double d = (b*b)-(4*a*c);
-		double x1 = ((-1*b) + (Math.sqrt(d))) / (2 * a);
-		double x2 = ((-1*b) - (Math.sqrt(d))) / (2 * a);
+/**
+ * Class for solution.
+ */
+final public class Solution {
+	/**
+	 * Constructor.
+	 */
+	private Solution(){
+		//unused
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      a     { first coefficient of the quadratic }
+	 * @param      b     { Second coefficient of the quadratic  }
+	 * @param      c     { Second coefficient of the quadratic  }
+	 */
+	void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+		double d = (b * b) - (4 * a * c);
+		double x1 = ((-1 * b) + (Math.sqrt(d))) / (2 * a);
+		double x2 = ((-1 * b) - (Math.sqrt(d))) / (2 * a);
 		System.out.println(x1 + " " + x2);
 	}
-	public static void main(String[] args) {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
@@ -20,7 +36,4 @@ public class Solution {
 		Solution s = new Solution();
 		s.rootsOfQuadraticEquation(a, b, c);
 	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
 }
