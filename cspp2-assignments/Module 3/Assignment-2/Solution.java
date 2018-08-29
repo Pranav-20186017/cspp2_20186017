3e7aff48adf78290cec0a13decaaf25e831031fd
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-29 10:24:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/Module 3/Assignment-2/Solution.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int check_seven(int n) {
+        int flag = 0;
+
+        while(n != 0)
+        {
+            int rem = n % 10;
+            if( rem == 7) {
+                flag ++;
+            }
+            n = n / 10;
+        }
+        return flag;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+
+        Scanner s = new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        int flag;
+        for(int i = 1; i <= n; i++) {
+            flag = check_seven(i);
+            count += flag;
+        }
+        System.out.println(count);
+        
+    }
+}
