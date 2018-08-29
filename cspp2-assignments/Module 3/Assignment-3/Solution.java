211830b310fab56cdbd26ea1dd1478ef59f4227a
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-29 09:56:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module 3/Assignment-3/Solution.java
+++ cspp2-assignments/Module 3/Assignment-3/Solution.java
@@ -10,12 +10,13 @@ final class Solution {
 		//unused.
 	}
     public static int gcd(int a, int b) {
-    	if (b==0) {
-            return a;
-        } 
-        else {
-            return gcd(b, b%a);
+        int gcd = 1;
+    	for (int i=1; i<=a && i<=b;i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
         }
+        return gcd; 
 
     }
     public static void main(String[] args) {
