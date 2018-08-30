04522c240eb0e20b624b95ba17e9694faaf3c49d
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-30 17:49:20 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2-assignments/Module 4/Assignment-3/Solution.java
+++ cspp2-assignments/Module 4/Assignment-3/Solution.java
@@ -3,7 +3,6 @@
 
 
 import java.util.Scanner;
-import java.lang.Math.*;
 public class Solution
 {/*
 	Do not modify this main function.
@@ -11,25 +10,16 @@ public class Solution
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		int n= Integer.parseInt(sc.nextLine());
+		int n= sc.nextInt();
 		for(int i=0;i<=n;i++){
 			String s=sc.nextLine();
 			String res=binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
-	public static String binaryToDecimal(String s) {
-		int max_pow = s.length()-1;
-		double val = 0;
-		for(int i=0;i<s.length();i++) {
-			char k = s.charAt(i);
-			int temp = Character.getNumericValue(k);
-			val = val + (temp * Math.pow(2,max_pow));
-			max_pow--;
-		}
-
-		long final_val = Math.round(val);
-		String ans = Long.toString(final_val);
-		return ans;
+	public static String binaryToDecimal(final String s)
+	{
+		return s;
 	}
+
 }
