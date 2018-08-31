b3ce6a65092821857b483bcc84f273607b33d4e5
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-31 09:32:05 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/Module 4/Assignment-3/Solution.java
+++ cspp2-assignments/Module 4/Assignment-3/Solution.java
@@ -15,8 +15,8 @@ final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= Integer.parseInt(sc.nextLine());
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i <= n - 1; i++){
             String s = sc.nextLine();
             long res = binaryToDecimal(s);//Write binaryToDecimal function
