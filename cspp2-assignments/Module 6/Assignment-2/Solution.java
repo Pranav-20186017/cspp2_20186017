565544c49464ac34c141243a68b2ec4ad9c5cf36
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-01 10:11:44 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module 6/Assignment-2/Solution.java
+++ cspp2-assignments/Module 6/Assignment-2/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  * Write a java program to round the
  * elements of a matrix to the nearest 100.
  *
- * @author : 
+ * @author : Pranav Surampudi
  */
 final class Solution {
     /**
@@ -22,7 +22,8 @@ final class Solution {
      *
      * @return     Matrix of the rounded elements
      */
-    static int[][] roundHundred(final int[][] a, final int rows, final int cols) {
+    static int[][] roundHundred(final int[][] a,
+     final int rows, final int cols) {
         final int h = 100;
         final int f = 50;
         for (int i=0; i < rows; i++) {
