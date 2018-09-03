60ef21f6fed9c31c99561a1b625bb99ecdda469d
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-03 12:14:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/Module 7/Assignment-1/Solution.java
+++ cspp2-assignments/Module 7/Assignment-1/Solution.java
@@ -37,7 +37,7 @@ public class Solution {
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         String input = s.next();
-        InputValidator i = new InputValidator(input);     
+        InputValidator i = new InputValidator(input);
         System.out.println(i.validateData());
     }
 }
