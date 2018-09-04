5f83c7b1169553ca48d7dda48205e20661b9ef35
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-04 15:31:40 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-assignments/Module 8/CodecampList/List.java
+++ cspp2-assignments/Module 8/CodecampList/List.java
@@ -166,8 +166,6 @@ final class List {
                 case "contains":
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
-                default:
-                System.out.println("Error Pick Something else");
             }
         }
     }
