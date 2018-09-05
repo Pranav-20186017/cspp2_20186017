fcdb7b63ec273892a5e0cfb7262448783572114d
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-05 10:19:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module 9/Assignment-1/Fibonacci.java
+++ cspp2-assignments/Module 9/Assignment-1/Fibonacci.java
@@ -1,24 +1,14 @@
 import java.util.Scanner;
-
-/*
- * In the previous module you have created List ADT.
- * ADTs are developed as reusable libraries
- * i.e., anyone who needs the ADT can use it in their program
- * A program that uses the ADT is called a "client" of the ADT
- * The client typically instantiates an object of the ADT
- * and invokes the public methods on the ADT object.
- *
- * This assignment is to practice the idea of client to the ADT.
- * Client class is Fibonacci and the client needs to use the List ADT
- * to store fibonacci numbers in the List ADT.
- * So, Fibonacci is going to instantiate an object of List ADT
- * generate fibonacci numbers and add them to the list
- * by invoking the add method provided in the List ADT.
- *
+/**
+ * Class for fibonacci.
  */
+public class Fibonacci {
+    /**
+     * Constructs the object.
+     */
+    private Fibonacci() {
 
-public class Fibonacci
-{
+    }
     /*
      * fib is a static method takes a argument n
      * n is the count of the fibonacci numbers to be generated.
@@ -26,7 +16,14 @@ public class Fibonacci
      *
      * Look for the hint.txt if some of the testcases fail.
      */
-    public static List fib(int n) {
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static List fib(final int n) {
         // todo - complete this method
         List l = new List(n);
         int a = 0;
@@ -42,7 +39,11 @@ public class Fibonacci
         }
         return l;
     }
-
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
 	public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
