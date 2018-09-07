2abed0aca0a33a8b7e32926935f6fe13eb855def
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-07 16:56:25 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/Module 11/Assignment-2/Solution.java
+++ cspp2-assignments/Module 11/Assignment-2/Solution.java
@@ -135,7 +135,7 @@ final class Solution {
                 case "subList":
                     if (tokens.length != 2) {
                         break;
-                    } 
+                    }
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(Integer.parseInt(
                     arrstring3[0]), Integer.parseInt(arrstring3[1]));
@@ -147,7 +147,7 @@ final class Solution {
                     if (tokens.length == 2) {
                         String[] lt = tokens[1].split(",");
                         List l2 = new List();
-                        for (int k = 0; k < lt.length; k++ ) {
+                        for (int k = 0; k < lt.length; k++) {
                             l2.add(Integer.parseInt(lt[k]));
                         }
                         System.out.println(l.equals(l2));
@@ -159,9 +159,14 @@ final class Solution {
                 default:
                 break;
             }
-        }   
+        } 
     }
-    public static void executeListDouble(Scanner stdin) {
+    /**
+     * { function_description }.
+     *
+     * @param      stdin  The stdin
+     */
+    public static void executeListDouble(final Scanner stdin) {
         List<Double> l = new List();
         while (stdin.hasNext()) {
             // read the line
