904dc202614ade2ab64bc9dd4b96dd4153464957
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-07 11:53:03 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 11/Assignment-1/List.java
+++ cspp2-assignments/Module 11/Assignment-1/List.java
@@ -413,7 +413,7 @@ final class List {
                         String[] t1 = tokens[1].split(",");
                         int[] temp = new int[t1.length];
                         for(int i = 0; i < temp.length; i++) {
-                            temp[i]=Integer.parseInt(t1[i]);
+                            temp[i] = Integer.parseInt(t1[i]);
                         }
                         l.addAll(temp);
                     }
@@ -422,7 +422,7 @@ final class List {
                     if (tokens.length == 2) {
                         String[] t2 = tokens[1].split(",");
                         int[] a = new int[t2.length];
-                        for(int i = 0; i < t2.length; i++) {
+                        for (int i = 0; i < t2.length; i++) {
                             a[i] = Integer.parseInt(t2[i]);
                         }
                         l.removeAll(a);
