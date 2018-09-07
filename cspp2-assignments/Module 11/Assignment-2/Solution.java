48b295079ba8e94068378309743df6f1a418722b
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-07 17:07:26 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/Module 11/Assignment-2/Solution.java
+++ cspp2-assignments/Module 11/Assignment-2/Solution.java
@@ -324,18 +324,20 @@ final class Solution {
                     if (tokens.length == 2) {
                         String[] t2 = tokens[1].split(",");
                         Float[] a = new Float[t2.length];
-                        for(int i = 0; i < t2.length; i++) {
+                        for (int i = 0; i < t2.length; i++) {
                             a[i] = Float.parseFloat(t2[i]);
                         }
                         l.removeAll(a);
                     }
                 break;
-                case "subList": 
+                case "subList":
                     if (tokens.length != 2) break;
                     String[] arrstring3 = tokens[1].split(",");
-                    List object = l.subList(Integer.parseInt(arrstring3[0]), Integer.parseInt(arrstring3[1]));
-                    if (object != null) 
+                    List object = l.subList(Integer.parseInt
+                    (arrstring3[0]), Integer.parseInt(arrstring3[1]));
+                    if (object != null) {
                         System.out.println(object);
+                    }
                     break;
                 case "equals":
                     if (tokens.length == 2) {
