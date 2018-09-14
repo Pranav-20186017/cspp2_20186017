c978d3a2899692741e13ab93c6a262e146f51aef
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-14 19:11:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module 17/Shoppingcart/Solution.java
+++ cspp2-assignments/Module 17/Shoppingcart/Solution.java
@@ -257,8 +257,10 @@ class ShoppingCart {
      */
     public void printInvoice() {
         System.out.println("Name   quantity   Price");
-        for (Item item : cart)
-            System.out.println(item.getName() + " " + item.getQuantity() + " " + getPrice(item.getName()));
+        for (Item item : cart) {
+            System.out.println(item.getName() + 
+            " " + item.getQuantity() + " " + getPrice(item.getName()));
+        }
         float total = getTotalAmount();
         System.out.println("Total:" + total);
         System.out.println("Disc%:" + 0.01 * coupon * total);
