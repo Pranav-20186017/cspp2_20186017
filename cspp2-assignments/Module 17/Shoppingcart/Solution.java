73751ebd8bd9c7a32c3969efc0281966f9f0aba2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-14 19:28:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/Module 17/Shoppingcart/Solution.java
+++ cspp2-assignments/Module 17/Shoppingcart/Solution.java
@@ -259,6 +259,7 @@ class ShoppingCart {
         final float pof = 0.15f;
         final int t = 10;
         final double ten = 10.0;
+        final double pzo = 0.01;
         System.out.println("Name   quantity   Price");
         for (Item item : cart) {
             System.out.println(item.getName() + " "
@@ -266,7 +267,7 @@ class ShoppingCart {
         }
         float total = getTotalAmount();
         System.out.println("Total:" + total);
-        System.out.println("Disc%:" + 0.01 * coupon * total);
+        System.out.println("Disc%:" + pzo * coupon * total);
         System.out.println("Tax:"
         + ((int) ((total * (1f - (0.01f * coupon)) * pof) * t)) / ten);
         System.out.println("Payable amount: "
