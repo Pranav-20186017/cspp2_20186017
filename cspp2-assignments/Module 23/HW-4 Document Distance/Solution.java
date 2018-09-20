0e46370fe7f1fd3f965a90cc417974425e0309b3
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-20 20:08:40 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/Module 23/HW-4 Document Distance/Solution.java
+++ cspp2-assignments/Module 23/HW-4 Document Distance/Solution.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 import java.io.File;
+/**
+ * Class for solution.
+ */
 public class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	Solution() {
+		//unused.
+	}
 	public static void main(String[] args) {
 		try {
 			Scanner scan = new Scanner(System.in);	
@@ -11,34 +20,32 @@ public class Solution {
 			String file1 = null;
 			String file2 = null; 
 			System.out.print("		");
-			for(int i=0;i<files.length-1;i++) {
-				System.out.print(files[i].getName()+"\t");
+			for(int i = 0; i < files.length- 1 ; i++) {
+				System.out.print(files[i].getName() + "\t");
 			}
 			System.out.print(files[files.length-1].getName());
 			System.out.println();
-			for(int i =0;i < files.length;i++) {
+			for(int i = 0; i < files.length; i++) {
 				System.out.print(files[i].getName()+"\t");
 				for(int j =0; j < files.length;j++) {
 					Distance map1 = new Distance(files[i]);
 					Distance map2 = new Distance(files[j]);
 					int similar = (int)Math.round(map1.angle(map2));
-					if(similar > maxVal && !files[i].getName().equals(files[j].getName())) {
+					if(similar > maxVal && 
+					!files[i].getName().equals(files[j].getName())) {
 						maxVal = similar;
 						file1 = files[i].getName();
 						file2 = files[j].getName();
 					}
-					System.out.print(similar+ "\t\t");
+					System.out.print(similar + "\t\t");
 				}
-				System.out.println();				
-				
+				System.out.println();
 			}
-			System.out.println("Maximum similarity is between "+file1+" and "+file2);
-			
+			System.out.println("Maximum similarity is between "
+			+ file1 + " and " + file2);
 			
 		} catch(Exception e) {
 			System.out.println("empty directory");
 		}
-		
 	}
-	
 }
